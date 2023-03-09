19af6ac73f66ce53ef33d6fb6aba29be4a6c76fd
quangtuan9997 <109707621+quangtuan9997@users.noreply.github.com>
2023-03-09 11:30:15 +0700

    Add files via upload

---
+++ thucHanh/src/KhaiBaoBien/Main.java
@@ -0,0 +1,16 @@
+package KhaiBaoBien;
+
+public class Main {
+    public static void main(String[] args) {
+        int i=10;
+        float f=20.5f;
+        boolean b=true;
+        char c='a';
+        String s="ha noi";
+        System.out.println("i ="+i);
+        System.out.println("f ="+f);
+        System.out.println("b ="+b);
+        System.out.println("c ="+c);
+        System.out.println("S ="+s);
+    }
+}
